db85c4589bb2d2b9291ab5ab364cac97d9751b7b
Roni Palmon <palmonroni@gmail.com>
2025-06-05 15:50:38 +0300

    book class + homeFragment

---
+++ app/src/main/java/com/example/bookreviewapp/Book.kt
@@ -0,0 +1,9 @@
+package com.example.bookreviewapp
+
+data class Book(
+    val id: Int,
+    val title: String,
+    val author: String,
+    val rating: Float,
+    val summary: String
+)
---
+++ app/src/main/java/com/example/bookreviewapp/HomeFragment.kt
@@ -0,0 +1,11 @@
+package com.example.bookreviewapp
+
+import androidx.fragment.app.Fragment
+
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        import androidx.fragment.app.Fragment
+        container : ViewGroup?
+    )
+}
