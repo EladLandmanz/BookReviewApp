3241cd54ebc96f40ed1e4005f03edc57022857f2
elad landman <elad.landman@post.runi.ac.il>
2025-06-09 17:51:44 +0300

    nav graph and fragments 1

--- app/src/main/java/com/example/bookreviewapp/ResultFragment.kt
+++ app/src/main/java/com/example/bookreviewapp/ResultFragment.kt
@@ -5,10 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.bookreviewapp.databinding.ResultFragmentBinding
 import com.example.bookreviewapp.databinding.SearchFragmentBinding
 
 class ResultFragment : Fragment() {
-    private var _binding : SearchFragmentBinding? = null
+    private var _binding : ResultFragmentBinding? = null
 
     private val binding get() = _binding!!
 
@@ -17,7 +18,7 @@ class ResultFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = SearchFragmentBinding.inflate(inflater,container,false)
+        _binding = ResultFragmentBinding.inflate(inflater,container,false)
         return binding.root
     }
 
