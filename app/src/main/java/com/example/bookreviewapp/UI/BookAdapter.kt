85680497760564c2adaacd4130a26d5f4ac54687
elad landman <elad.landman@post.runi.ac.il>
2025-06-11 15:11:18 +0300

    added temp book adapter

---
+++ app/src/main/java/com/example/bookreviewapp/UI/BookAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.bookreviewapp.UI
+
+import androidx.recyclerview.widget.RecyclerView
+
+
+class BookAdapter(private val ) {
+
+    class BookViewHolder(private val binding: ) : RecyclerView.ViewHolder(binding.root){
+
+    }
+}
